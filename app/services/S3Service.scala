8e45991c0e04a6d5bf17fd33a90ab3b684445065
Keisuke Suda <ksuda940sc@gmail.com>
2016-11-10 17:59:28 +0900

    S3Serviceを作成する

---
+++ app/services/S3Service.scala
@@ -0,0 +1,46 @@
+package services
+
+import java.io.File
+
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.util.IOUtils
+import com.typesafe.config.ConfigFactory
+import utils.Using
+
+import scala.util.{Failure, Success, Try}
+
+object S3Service {
+  private val config = ConfigFactory.parseFile(new File("./conf/application.conf")).resolve
+  private val accessKey  = config.getString("aws.s3.accesskey")
+  private val secretKey  = config.getString("aws.s3.secretkey")
+  private val bucketName = config.getString("aws.s3.bucketname")
+  private val s3 = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey))
+
+  def upload(prefix: String, name: String, file: File): Boolean = {
+    val objectKey = prefix + name
+
+    Try(s3.putObject(bucketName, objectKey, file)) match {
+      case Success(result) => hasObject(objectKey)
+      case Failure(t) => false
+    }
+  }
+
+  def download(objectKey: String): Option[Array[Byte]] = {
+    val obj = Try(s3.getObject(bucketName, objectKey))
+
+    obj match {
+      case Success(result) => for (in <- Using(obj.get.getObjectContent)) Some(IOUtils.toByteArray(in))
+      case Failure(t) => None
+    }
+  }
+
+  def delete(objectKey: String): Boolean = {
+    Try(s3.deleteObject(bucketName, objectKey)) match {
+      case Success(result) => !hasObject(objectKey)
+      case Failure(t) => false
+    }
+  }
+
+  def hasObject(objectKey: String): Boolean = s3.doesObjectExist(bucketName, objectKey)
+}
